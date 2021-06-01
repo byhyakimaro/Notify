31fa17a700c20092c1c3989e0a61435026cddb48
hyakimaro <>
2021-06-01 23:37:57 +0000

    OK

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Example FactoryObject
+
 const bg_notificacao = $('.bg-notificacao');
 const notificacao = $('.notificacao');
 const texto = $('.bg-notificacao .notificacao .texto');
 const icon = $('.bg-notificacao .notificacao .icon svg');
 
-async function notification() {
+async function createNotify(color = "#77b901") {
+	bg_notificacao.css('background',color);
+	icon.css('fill', color);
 	bg_notificacao.style.transform = "translateX(240px)";
 	notificacao.style.transform = "translateX(240px)";
 	await sleep(1000);
@@ -11,6 +15,11 @@ async function notification() {
 	notificacao.style.transform = "translateX(4px)";
 	texto.style.opacity = "100%";
 	icon.style.opacity = "100%";
+	return {
+		color,
+	}
 }
 
-notification();
+(async()=>{
+	const addNotify = await createNotify();
+})();
